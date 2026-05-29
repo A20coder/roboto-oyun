daed23659f2128008a6381f4815ad61578f50497
A20coder <polic.3304@gmail.com>
2026-05-29 18:51:44 +0300

    Initial commit

---
+++ roboto-android/main.py
@@ -0,0 +1,6 @@
+# Android (buildozer / python-for-android) giriş noktası.
+# Telefonda uygulama açıldığında bu dosya çalıştırılır.
+from game import Game
+
+if __name__ == "__main__":
+    Game().run()
