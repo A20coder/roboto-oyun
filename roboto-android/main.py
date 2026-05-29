48ad5bc043216a34c69b7ba81f47895d74e073bb
ASLAN <usta.kimyaci@hotmail.com>
2026-05-29 18:54:46 +0300

    Gereksiz roboto-android alt klasorunu kaldir

--- roboto-android/main.py
+++
@@ -1,6 +0,0 @@
-# Android (buildozer / python-for-android) giriş noktası.
-# Telefonda uygulama açıldığında bu dosya çalıştırılır.
-from game import Game
-
-if __name__ == "__main__":
-    Game().run()
